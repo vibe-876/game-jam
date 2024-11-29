242cf6414e36619eedae6736b8b245fc416734e5
cam <cameronayres123@gmail.com>
2024-11-29 19:49:01 +0000

    Added title screen options.

--- main.c
+++ main.c
@@ -11,21 +11,41 @@ int main(int argc, char **argv) {
 
 void title_screen() {
   int row, col;
-  char title[] = "Tax Evasion Over Telnet!";
-  char pak[] = "(press any key)";
+  char mode;
+  char title_msg[] = "Tax Evasion Over Telnet!";
+  char play_msg[] = "[p] => play game";
+  char tutorial_msg[] = "[t] => play tutorial";
+  char credits_msg[] = "[c] => show credits";
+  char quit_msg[] = "[q] => quit";
   
   initscr();
   getmaxyx(stdscr, row, col);
 
   attron(A_BOLD);
-  mvprintw(row / 2, (col - strlen(title)) / 2,
-	   "%s", title);
+  mvprintw(row / 2, (col - strlen(title_msg)) / 2,
+	   "%s", title_msg);
   attroff(A_BOLD);
-  
-  mvprintw((row / 2) + 1, (col - strlen(title)) / 2,
-	   "%s", pak);
 
+  mvprintw((row / 2) + 2, (col - strlen(play_msg)) / 2,
+	   "%s", play_msg);
+  mvprintw((row / 2) + 3, (col - strlen(play_msg)) / 2,
+	   "%s", tutorial_msg);
+  mvprintw((row / 2) + 4, (col - strlen(play_msg)) / 2,
+	   "%s", credits_msg);
+  mvprintw((row / 2) + 5, (col - strlen(play_msg)) / 2,
+	   "%s", quit_msg);
+  
+  
+  switch(mode = getch()) {
+  case 'q':
+    goto exit;
+  default:
+    break;
+  }
+  
+ exit:
   refresh();
   getch();
   endwin();
+  return;
 }

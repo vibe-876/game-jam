5b10112a147e39ba6df4a56c454552500c74592d
cam <cameronayres123@gmail.com>
2024-11-29 19:31:06 +0000

    Added title screen.

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
+#include "main.h"
 #include <ncurses.h>
+#include <string.h>
 
 
 int main(int argc, char **argv) {
+  title_screen();
+
   return(0);
+}
+
+void title_screen() {
+  int row, col;
+  char message[] = "Tax Evasion Over Telnet!";
+  
+  initscr();
+  getmaxyx(stdscr, row, col);
+
+  mvprintw(row / 2, (col - strlen(message)) / 2,
+	   "%s", message);
+
+  refresh();
+  getch();
+  endwin();
 }

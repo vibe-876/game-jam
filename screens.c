21c2fb5182dc4356c234e90e2a2e553fe8912f3e
cam <cameronayres123@gmail.com>
2024-12-01 05:57:22 +0000

    Updated title_screen so that it uses the new functions.

--- screens.c
+++ screens.c
@@ -4,30 +4,18 @@
 
 
 void title_screen() {
-  int row, col;
-  char mode;
-  char title_msg[] = "Tax Evasion Over Telnet!";
-  char play_msg[] = "[p] => play game";
-  char tutorial_msg[] = "[t] => play tutorial";
-  char credits_msg[] = "[c] => show credits";
-  char quit_msg[] = "[q] => quit";
+  char mode[2];
+  char *options[] = {"[p] => play game", "[t] => play tutorial", "[c] => show credits", "[q] => quit", 0x0};
+  mode[1] = 0x0;
     
   while(1) {
-    getmaxyx(stdscr, row, col);
-    row = (row / 2) - 5;
     clear();
     
     attron(A_BOLD);
-    mvprintw(row, (col - strlen(title_msg)) / 2, "%s", title_msg);
+    game_message("Tax Evasion Over Telnet!", -6);
     attroff(A_BOLD);
-
-    col = (col - strlen(play_msg)) / 2;
-    mvprintw(row + 2, col, "%s", play_msg);
-    mvprintw(row + 3, col, "%s", tutorial_msg);
-    mvprintw(row + 4, col, "%s", credits_msg);
-    mvprintw(row + 5, col, "%s", quit_msg);
   
-    switch(mode = getch()) {
+    switch(mode[0] = game_options(options, -4)) {
     case 'c':
       clear();
   
@@ -48,7 +36,8 @@ void title_screen() {
       goto exit;
       
     case 't':
-      fullscreen_message("launching tutorial...", -5);
+      fullscreen_message("tutorial not implemented yet :( .", -5);
+      
       break;
       
     default:
